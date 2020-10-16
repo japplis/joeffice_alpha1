1a56d446846593e016ee10e519c161771ae5d612
Anthony Goubard <anthony.goubard@japplis.com>
2020-10-16 11:12:47 +0200

    Migrated code from bitbucket (Mercurial) to GitHub (Git) - no history kept

---
+++ WordProcessor/src/org/joeffice/wordprocessor/package-info.java
@@ -0,0 +1,6 @@
+@TemplateRegistration(folder = "Office", content = "DocxTemplate.docx", displayName = "#LBL_Docx_FORMAT", iconBase = "org/joeffice/wordprocessor/wordprocessor-16.png", position = 100, description = "DocxDescription.html", requireProject = false)
+@NbBundle.Messages("LBL_Docx_FORMAT=Word processor")
+package org.joeffice.wordprocessor;
+
+import org.netbeans.api.templates.TemplateRegistration;
+import org.openide.util.NbBundle;
